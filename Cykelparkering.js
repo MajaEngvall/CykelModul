694b40b34903920bd4f3c68ba4f38307b19cb295
isakkabir <jisan93@hotmail.com>
2015-04-15 17:01:23 +0200

    ändrat

--- Cykelparkering.js
+++ Cykelparkering.js
@@ -25,21 +25,21 @@ var parkering = [{
     "description":"PLATSER: 12"
 
 },{
-    "name":""Länsstyrelsen"",
+    "name":"Länsstyrelsen",
     "latitude":59.853361,
     "longitude":17.645422,
     "description":"PLATSER: 28"
 
 },{
-    "name":Östra ågatan, mellan Bredgränd och Smedsgränd",
+    "name":"Östra ågatan mellan Bredgränd och Smedsgränd",
     "latitude":59.857418,
     "longitude":17.639060,
     "description":"PLATSER: 137"
 }, {
-    "name":Östra ågatan, mellan Bredgränd och Smedsgränd",
+    "name":"Östra ågatan mellan Bredgränd och Smedsgränd",
     "latitude":59.857418,
     "longitude":17.639060,
-    "description":PLATSER: 18"
+    "description":"PLATSER: 18"
 },{
     "name":"Bangårdsgatan\/Dragarbrunnsgatan",
     "latitude":59.857402,
@@ -62,48 +62,48 @@ var parkering = [{
     "name":"Dragarbrunnsg. \/ Vretgränd",
     "latitude":59.857035,
     "longitude": 17.644149,
-    "description":PLATSER: 20"
+    "description":"PLATSER: 20"
 
 },{
     "name": "Strandbodgatan (KV.Munin)  Östra",
     "latitude":59.852443,
     "longitude":17.646858,
-    "description":"PLATSER":168"
+    "description":"PLATSER:168"
 
 },{
     "name":"Strandbodgatan (Bostadshus)",
     "latitude":59.853427,
     "longitude":17.649040,
-    "description "PLATSER":24"
+    "description": "PLATSER:24"
 
 },{
     "name":"Strandbodgatan (Bostadshus)",
-        "latitude":59.853124,
+    "latitude":59.853124,
     "longitude":17.648382,
-    "description "PLATSER":20"
+    "description": "PLATSER:20"
 },{
     "name":"Strandbodgatan, Mäklarhuset",
-        "latitude":59.854318,
-        "longitude":17.649937,
-        "description "PLATSER:24"
+    "latitude":59.854318,
+    "longitude":17.649937,
+    "description": "PLATSER:24"
 }, {
     "name":"Ebba Boströmsgata",
-        "latitude":59,854289,
-        "longitude":17,647921,
-        "description "PLATSER:130"
+    "latitude":59.854289,
+    "longitude":17.647921,
+    "description": "PLATSER:130"
 }, {
     "name":"Kungsängstorg",
-        "latitude":59,854365,
-        "longitude":17,647764,
-        "description "PLATSER:9"
+    "latitude":59.854365,
+    "longitude":17.647764,
+    "description": "PLATSER:9"
 }, {
     "name":"Kungsängsg. viadukt Livsmedelsv. Ö",
-        "latitude":59,853818,
-        "longitude":17,647523,
-        "description "PLATSER:39"
+    "latitude":59.853818,
+    "longitude":17.647523,
+    "description": "PLATSER:39"
 },{
-    "name":,"Kungsängsgatan viadukt Livsmedelsv. V"
-        "latitude":59,853713,
-        "longitude":17,647235,
-        "description "PLATSER:34"
-}
+    "name":"Kungsängsgatan viadukt Livsmedelsv. V",
+    "latitude":59.853713,
+    "longitude":17.647235,
+    "description":"PLATSER:34"
+}];
--- pump.js
+++ pump.js
@@ -795,16 +795,128 @@ var pump = [{
         "email":""
     }];
 
+
+var PLATSER;
+var parkering = [{
+    "name":"Bangårdsgatan\/Dragarbrunnsgatan",
+    "latitude":59.857726,
+    "longitude":17.643432,
+    "description":"PLATSER: 28"
+},{
+    "name":"Sturegatan\/Skolgatan",
+    "latitude":59.861417,
+    "longitude":17.629425,
+    "description":"PLATSER: 30"
+
+}, {
+    "name":"S:t Johannesgatan\/Sysslomansgatan",
+    "latitude":59.859783,
+    "longitude":17.629772,
+    "description":"PLATSER: 70"
+
+},{
+    "name":"Skolgatan\/Sysslomansgatan, västra",
+    "latitude":59.860739,
+    "longitude":17.627888,
+    "description":"PLATSER: 12"
+
+},{
+    "name":"Länsstyrelsen",
+    "latitude":59.853361,
+    "longitude":17.645422,
+    "description":"PLATSER: 28"
+
+},{
+    "name":"Östra ågatan mellan Bredgränd och Smedsgränd",
+        "latitude":59.857418,
+    "longitude":17.639060,
+    "description":"PLATSER: 137"
+}, {
+    "name":"Östra ågatan mellan Bredgränd och Smedsgränd",
+    "latitude":59.857418,
+        "longitude":17.639060,
+        "description":"PLATSER: 18"
+},{
+    "name":"Bangårdsgatan\/Dragarbrunnsgatan",
+        "latitude":59.857402,
+        "longitude":17.642625,
+        "description":"PLATSER: 28"
+
+},{
+    "name":"Luthagsesplanaden \/ Börjegatan",
+        "latitude":59.861995,
+        "longitude":17.622327,
+        "description":"PLATSER: 25"
+
+},{
+    "name":"Gustavianum, kortsida",
+        "latitude":59.858246,
+        "longitude":17.631651,
+        "description":"PLATSER: 16"
+
+},{
+    "name":"Dragarbrunnsg. \/ Vretgränd",
+        "latitude":59.857035,
+        "longitude": 17.644149,
+        "description":"PLATSER: 20"
+
+},{
+    "name": "Strandbodgatan (KV.Munin)  Östra",
+        "latitude":59.852443,
+        "longitude":17.646858,
+        "description":"PLATSER:168"
+
+},{
+    "name":"Strandbodgatan (Bostadshus)",
+        "latitude":59.853427,
+        "longitude":17.649040,
+        "description": "PLATSER:24"
+
+},{
+    "name":"Strandbodgatan (Bostadshus)",
+        "latitude":59.853124,
+        "longitude":17.648382,
+        "description": "PLATSER:20"
+},{
+    "name":"Strandbodgatan, Mäklarhuset",
+        "latitude":59.854318,
+        "longitude":17.649937,
+        "description": "PLATSER:24"
+}, {
+    "name":"Ebba Boströmsgata",
+        "latitude":59.854289,
+        "longitude":17.647921,
+        "description": "PLATSER:130"
+}, {
+    "name":"Kungsängstorg",
+        "latitude":59.854365,
+        "longitude":17.647764,
+        "description": "PLATSER:9"
+}, {
+    "name":"Kungsängsg. viadukt Livsmedelsv. Ö",
+        "latitude":59.853818,
+        "longitude":17.647523,
+        "description": "PLATSER:39"
+},{
+    "name":"Kungsängsgatan viadukt Livsmedelsv. V",
+    "latitude":59.853713,
+        "longitude":17.647235,
+        "description":"PLATSER:34"
+    }];
+
+
 var week=["monday", "tuesday", "wednesday", "thursday", "friday", "saturday", "sunday"];
 var map;
 var pumpMarker=[];
 var verkstadMarker=[];
+var parkeringMarker=[];
 
 
 function onloading(){
     initialize();
     createMarkerArray(pump, pumpMarker,'http://maps.google.com/mapfiles/ms/icons/yellow-dot.png' );
     createMarkerArray(verkstad, verkstadMarker, 'http://maps.google.com/mapfiles/ms/icons/purple-dot.png');
+    createMarkerArray(parkering, parkeringMarker, 'http://maps.google.com/mapfiles/ms/icons/green-dot.png');
 
     var date = new Date();
     var day = date.getDay();
@@ -887,3 +999,9 @@ function checkVerkstad(){
         removeMarker(verkstadMarker);
     }
 }
+function checkParkering(){
+    if( document.getElementById("checkParkering").checked==true){
+        addMarker(parkeringMarker);
+    }else{
+        removeMarker(parkeringMarker);
+    }}
