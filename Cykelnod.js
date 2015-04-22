5a03d9575cc29c5a62fd70fc404fd2d9d8631f60
adamafugglas <adam.afugglas@gmail.com>
2015-04-22 10:29:51 +0200

    Extra nod resecentrum

--- Cykelnod.js
+++ Cykelnod.js
@@ -6,10 +6,13 @@ var cykelnoder = [{
     "name":"Daghammarsköldsgatan 31",
     "latitude":59.844146,
     "longitude":17.638019
-
 },{
     "name":"Hamnspången",
     "latitude":59.851526,
     "longitude":17.645485
+},{
+    "name":"Resecentrum",
+    "latitude":59.858162,
+    "longitude":17.644907
 }];
 
